cd5ab1764ac770faadf4e926406b1ccfabbc6437
ipinlearn4code <220605110103@student.uin-malang.ac.id>
2025-07-08 15:46:44 +0700

    Update permission matrix icons to enhance visual feedback for permission status

--- resources/views/filament/pages/permission-matrix.blade.php
+++ resources/views/filament/pages/permission-matrix.blade.php
@@ -77,7 +77,7 @@
                                                             <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
                                                         </svg>
                                                     @else
-                                                        <svg class="h-6 w-6 transition-colors duration-200" style="color: #d1d5db;" viewBox="0 0 24 24" fill="currentColor" onmouseover="this.style.color='#9ca3af'" onmouseout="this.style.color='#d1d5db'">
+                                                        <svg class="h-6 w-6 transition-colors duration-200" style="color: #ff0500;" viewBox="0 0 24 24" fill="currentColor" onmouseover="this.style.color='#cc0400'" onmouseout="this.style.color='#ff0500'">
                                                             <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd" />
                                                         </svg>
                                                     @endif
@@ -105,7 +105,7 @@
                         <span class="text-sm text-gray-600 dark:text-gray-400">Permission Granted</span>
                     </div>
                     <div class="flex items-center space-x-3">
-                        <svg class="h-5 w-5" style="color: #d1d5db;" viewBox="0 0 24 24" fill="currentColor">
+                        <svg class="h-5 w-5" style="color: #ff0500;" viewBox="0 0 24 24" fill="currentColor">
                             <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z" clip-rule="evenodd" />
                         </svg>
                         <span class="text-sm text-gray-600 dark:text-gray-400">Permission Denied</span>
